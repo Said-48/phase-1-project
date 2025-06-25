4d44a14308e5455535aa3c69c9bd9e0eb4e6e329
saidabdulhakim48@gmail.com <saidabdulhakim48@gmail.com>
2025-06-25 22:53:12 +0300

    books displayed

--- index.js
+++ index.js
@@ -32,3 +32,36 @@ async function fetchBooks(query) {
         return [];
     }
 }
+
+// display books 
+function displayBooks(books) {
+    booksContainer.innerHTML = "";
+
+    books.forEach((book) => {
+        const bookCard = document.createElement("div");
+        bookCard.className = "book-card";
+
+        const title = book.title || "No title";
+        const priceKES = 500
+        const author = book.author_name ? book.author_name.join(", ") : "Unknown";
+        const coverId = book.cover_i;
+        const imageUrl = coverId
+            ? `https://covers.openlibrary.org/b/id/${coverId}-M.jpg`
+            : "https://via.placeholder.com/150x200?text=No+Cover";
+
+        bookCard.innerHTML = `
+            <img src="${imageUrl}" alt="${title}" width="150" height="200"/>
+            <h3>${title}</h3>
+            <p><i>${author}</i></p>
+            <p>Price: KES ${priceKES}</p>
+            <button class="add-to-cart">Add to Cart</button>
+        `;
+
+        // Add eventlistener for cart button
+        bookCard.querySelector(".add-to-cart").addEventListener("click", () => {
+            addToCart(title);
+        });
+
+        booksContainer.appendChild(bookCard);
+    });
+}
